814e7dbd4480920d2149fd54c31880a75068c5d8
DimitryGonzales <dimitry.alves@sga.pucminas.br>
2026-08-23 15:25:28 -0300

    Formatar atividade 1

--- 1/src/Main.java
+++ 1/src/Main.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int fatorial(int valor) {
-        return valor == 1 ? 1 : valor * fatorial(valor - 1);
-    }
+    public static int fatorial(int valor) { return valor == 1 ? 1 : valor * fatorial(valor - 1); }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
