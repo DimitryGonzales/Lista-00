67664f74623bf636d7381f56884bbb1896ff0d8c
DimitryGonzales <dimitry.alves@sga.pucminas.br>
2026-08-23 18:19:56 -0300

    Formatar atividade 7

--- 7/src/Main.java
+++ 7/src/Main.java
@@ -2,20 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static String mesNome(int mesNumero) {
-        String[] meses = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+        String[] meses = {
+                "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+                "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+        };
 
         return meses[mesNumero - 1];
-    }
-
-    public static int[] gerarTemperaturas(Scanner sc) {
-        int[] temperaturas = new int[12];
-
-        for (int i = 0; i < temperaturas.length; i++) {
-            System.out.printf("Digite a temperatura média de %s: ", mesNome(i + 1));
-            temperaturas[i] = sc.nextInt();
-        }
-
-        return temperaturas;
     }
 
     public static int[] encontrarMaiorTemperatura(int[] temperaturas) {
@@ -47,12 +39,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] temperaturas = gerarTemperaturas(sc);
+        int[] temperaturas = new int[12];
+
+        for (int i = 0; i < temperaturas.length; i++) {
+            System.out.printf("Digite a temperatura média de %s: ", mesNome(i + 1));
+            temperaturas[i] = sc.nextInt();
+        }
+
         int[] temperaturaMaiorMes = encontrarMaiorTemperatura(temperaturas);
         int[] temperaturaMenorMes = encontrarMenorTemperatura(temperaturas);
 
         System.out.printf(
-                "Maior temperatura: %d° - %s\nMenor temperatura: %d° - %s",
+                "\nMaior temperatura: %d° - %s\nMenor temperatura: %d° - %s",
                 temperaturaMaiorMes[0], mesNome(temperaturaMaiorMes[1]),
                 temperaturaMenorMes[0], mesNome(temperaturaMenorMes[1])
         );
