1dc1644478bf7ac5a519a9e66e1a731d7bf505fe
DimitryGonzales <dimitry.alves@sga.pucminas.br>
2026-08-23 15:51:55 -0300

    Refatorar atividade 3

--- 3/src/Main.java
+++ 3/src/Main.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static int[] receberMatriculas(Scanner sc) {
-        System.out.print("Digite a quantidade de matriculas: ");
+        System.out.print("\tDigite a quantidade de matriculas: ");
         int quantidade = sc.nextInt();
 
         int[] matriculas = new int[quantidade];
 
         for (int i = 0; i < quantidade; i++) {
-            System.out.printf("Digite o número da matrícula %d: ", i + 1);
+            System.out.printf("\tMatrícula %d: ", i + 1);
             matriculas[i] = sc.nextInt();
         }
 
@@ -21,23 +21,14 @@ public class Main {
         System.out.println("Programação Modular: ");
         int[] matriculasProgMod = receberMatriculas(sc);
 
-        System.out.println("Cálculo: ");
+        System.out.println("\nCálculo: ");
         int[] matriculasCalculo = receberMatriculas(sc);
 
-        int[] vetorMaior, vetorMenor;
-
-        if (matriculasProgMod.length > matriculasCalculo.length) {
-            vetorMaior = matriculasProgMod;
-            vetorMenor = matriculasCalculo;
-        } else {
-            vetorMaior = matriculasCalculo;
-            vetorMenor = matriculasProgMod;
-        }
-
-        for (int i = 0; i < vetorMaior.length;i++) {
-            for (int j = 0; j < vetorMenor.length; j++) {
-                if (vetorMaior[i] == vetorMenor[j])
-                    System.out.printf("A matricula %d está presente nas duas disciplinas\n", vetorMaior[i]);
+        System.out.print("\nMatriculas presentes em ambas as disciplinas: ");
+        for (int matriculaProgMod : matriculasProgMod) {
+            for (int matriculaCalculo : matriculasCalculo) {
+                if (matriculaProgMod == matriculaCalculo)
+                    System.out.printf("%d ", matriculaCalculo);
             }
         }
     }
